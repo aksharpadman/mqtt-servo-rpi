deec62eb7d607cd4df51c223a150831ff79400b6
aksharpadman <aksharpadman@gmail.com>
2017-12-18 17:28:03 +0000

    test2

--- servorpi.py
+++ servorpi.py
@@ -4,19 +4,23 @@ import json
 from time import sleep
 THINGSBOARD_HOST = 'demo.thingsboard.io'
 ACCESS_TOKEN = 'UeugaVs0MDlymrwT21GK'
+#Set GPIO pins numberingg as on RPi board
 GPIO.setmode(GPIO.BOARD)
+#Use pin 3 as output
 GPIO.setup(3,GPIO.OUT)
+#set pin for PWM
 pwm=GPIO.PWM(3,50)
 pwm.start(0)
-
+#function called when connected to server
 def On_connect(client, userdata, rc, *extra_params):
     print('Connected with result code ' + str(rc))
     client.subscribe('v1/devices/me/rpc/request/+')
 
+#function for processing the control message
 def On_message(client, userdata, msg):
     data = json.loads(msg.payload)
     SetAngle(data)
-
+#function for turning the servomotor as per requirement
 def SetAngle(angle):
      duty=(angle/18)+2
      GPIO.output(3,True)
